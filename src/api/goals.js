2b9512a3054968b459f7699f0cb1063837210d42
KONDO <kondon.94.j@gmail.com>
2025-12-07 17:32:40 +0900

    APIのURLを変更

--- src/api/goals.js
+++ src/api/goals.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const createGoal = (data) => {
-  return axios.post("http://localhost/api/goals", data);
+  return axios.post("http://127.0.0.1:8000/api/goals", data);
 };
